5b4dd940400fba85307348def0f74bc1787d6dfa
IgorLimaKI <igorlimacmp@gmail.com>
2019-10-23 00:19:47 -0300

    Iniciando implementação algoritmo genetico

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 from simulated_annealing.SimulatedAnnealing import SimulatedAnnealing
+from algoritmo_genetico.Algoritmo_Genetico import AlgoritmoGenetico
 from tabuleiro.Tabuleiro import Tabuleiro
 import funcao_objetiva.FuncaoObjetiva as func
 import os
@@ -11,14 +12,15 @@ numRainhas = 0
 while numRainhas < dimensao:
     os.system('cls' if os.name == 'nt' else 'clear')
     newTabuleiro.printTabuleiro()
-    print('Rainha ', numRainhas+1)
+    print('Rainha ', numRainhas + 1)
     posX = int(input('Digite a linha da rainha : '))
     posY = numRainhas
-    newTabuleiro.setRainha(posX-1, posY)
-    numRainhas+=1
+    newTabuleiro.setRainha(posX - 1, posY)
+    numRainhas += 1
 
 os.system('cls' if os.name == 'nt' else 'clear')
 newTabuleiro.printTabuleiro()
 print(func.f(newTabuleiro))
-s = SimulatedAnnealing(newTabuleiro,50,100)
-s.execute(func.f)
+#s = SimulatedAnnealing(newTabuleiro, 50, 100)
+#s.execute(func.f)
+a = AlgoritmoGenetico(newTabuleiro, 200, 100, 50, dimensao)
--- algoritmo_genetico/Algoritmo_Genetico.py
+++ algoritmo_genetico/Algoritmo_Genetico.py
@@ -1,23 +1,39 @@
+from tabuleiro.Tabuleiro import Tabuleiro
+import funcao_objetiva.FuncaoObjetiva as func
+import math
+import random
+import numpy as np
+
+
+# genótipo = colunas, exemplo [3,1,2] - rainha 1 na linha 3, rainha 2 na linha 1 e rainha 3 na linha 2
 class AlgoritmoGenetico:
-    
-    def __init__(self, populacao, candidatos):
-        self.populacao = populacao
-        self.candidatos = candidatos
+    def __init__(self, configInicial: Tabuleiro, maxIteracoes, maxPopulacao, taxaMutacao, tamanhoTabuleiro):
+        self.configInicial = configInicial
+        self.maxIteracoes = maxIteracoes
+        self.maxPopulacao = maxPopulacao
+        self.taxaMutacao = taxaMutacao
+        self.configAtual = configInicial
+        self.tamanhoTabuleiro = tamanhoTabuleiro
+        self.populacao = self.criaPopulacao()
 
-    def mutacao(self, individuo):
-        print()
+    def printTabuleiro(self):
+        print(self.configAtual)
 
-    def cruzamento(self, candidato1, candidato2):
-        print()
-    
-    def selecao(self, populacao):
-        print()
-    
-    def avaliacao(self, candidato):
-        print()
+    def criaCromossomo(self):
+        rainhas = np.arange(self.tamanhoTabuleiro)
+        np.random.shuffle(rainhas)
+        return rainhas
 
-    def codificacao(self, individuo):
-        print()
+    def avaliarCromossomo(self, cromossomo):
+        novoTabuleiro = Tabuleiro(self.tamanhoTabuleiro)
+        for i, cromo in enumerate(cromossomo):
+            novoTabuleiro.setRainha(cromo, i)
+        return func.f(novoTabuleiro)
 
-    def execute(self, f):
-        print()
+    def criaPopulacao(self):
+        population = []
+        for i in range(self.maxPopulacao):
+            tabuleiro = self.criaCromossomo()
+            avaliacao = self.avaliarCromossomo(tabuleiro)
+            population.append([tabuleiro, avaliacao])
+        return population
